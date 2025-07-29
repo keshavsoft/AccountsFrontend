d9405626fa0463be50514513e82339da0dfdafc5
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-07-29 16:03:30 +0530

    JournalsTable insertd new

---
+++ src/Js/JournalsTable/Delete/FormLoad/BuildBsTable/entryFile.js
@@ -0,0 +1,14 @@
+import { StartFunc as StartFuncOnClickRowFunc } from "./onClickRow/entryFile.js";
+
+const StartFunc = () => {
+    const $table = $('#table');
+
+    const finalOptions = {
+        onClickRow: StartFuncOnClickRowFunc
+    };
+
+
+    $table.bootstrapTable(finalOptions);
+};
+
+export { StartFunc };
