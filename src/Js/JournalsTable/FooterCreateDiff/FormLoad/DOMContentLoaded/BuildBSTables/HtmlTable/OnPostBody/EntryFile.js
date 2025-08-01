8e3b47384b3c87175702219d6e81f9ba48f6004a
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2025-08-01 16:01:54 +0530

    Some changes

--- src/Js/JournalsTable/FooterCreateDiff/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/EntryFile.js
+++ src/Js/JournalsTable/FooterCreateDiff/FormLoad/DOMContentLoaded/BuildBSTables/HtmlTable/OnPostBody/EntryFile.js
@@ -1,9 +1,5 @@
-import { StartFunc as StartFuncTableFooterSaveButtonId } from "./TableFooterSaveButtonId/EntryFile.js";
-
 let StartFunc = () => {
-    StartFuncTableFooterSaveButtonId();
  
-    table.querySelector("tfoot input").focus();
 };
 
 export { StartFunc };
